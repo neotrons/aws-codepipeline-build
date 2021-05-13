701315f8c3bf5645303bef76f3f3f82da1f3f1b0
Jose Ramirez <jcramireztello@gmail.com>
2021-05-13 16:53:31 -0500

    feat: add code deploy

---
+++ bin/codedeploy/before_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf /home/ubuntu/app/
+mkdir /home/ubuntu/app/
+chown ubuntu:ubuntu /home/ubuntu/app/
--- bin/copy.sh
+++ bin/copy.sh
@@ -2,4 +2,5 @@
 
 cp -ra ./server/ ./dist/aws-codepipeline-build/
 cp -ra ./bin/ ./dist/aws-codepipeline-build/
+cp -ra ./aws/appspec.yml ./dist/aws-codepipeline-build/
 echo "Archivos copiados"
